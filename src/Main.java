559fc4e7d71f7e40358d2008df63aa38846dce50
Dionysis <pasenidis.dionisios@gmail.com>
2021-11-24 13:46:42 +0200

    Add SQL Query for Student Schedule

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
 import views.StudentPanel;
 
 public class Main {
 
-  public static void main(String[] args)
-      throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
-    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    public static void main(String[] args)
+            throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
-    SwingUtilities.invokeLater(() -> {
-      StudentPanel home = new StudentPanel();
-      home.setVisible(true);
-    });
-  }
+        SwingUtilities.invokeLater(() -> {
+            StudentPanel home = new StudentPanel();
+            home.setVisible(true);
+        });
+    }
 }
--- src/views/StudentPanel.java
+++ src/views/StudentPanel.java
@@ -8,16 +8,21 @@ import javax.swing.table.DefaultTableModel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class StudentPanel extends JFrame {
 
     private JTable scheduleTable;
     private JPanel studentPanel;
 
-    private static String dbURL = "jdbc:postgresql://localhost:5432/postgres";
-    private static String dbUser = "postgres";
-    private static String dbPass = "kekw123";
+    private static final String dbURL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String dbUser = "postgres";
+    private static final String dbPass = "kekw123";
     private static Connection dbConnection;
+    private static Statement dbStatement;
+    private static ResultSet dbResult;
 
     public StudentPanel() {
         add(studentPanel);
@@ -27,27 +32,59 @@ public class StudentPanel extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
+    public String getDay(int day) {
+        return switch (day) {
+            case 0 -> "Monday";
+            case 1 -> "Tuesday";
+            case 2 -> "Wednesday";
+            case 3 -> "Thursday";
+            case 4 -> "Friday";
+            default -> "N/A";
+        };
+    }
+
+    public String getTime(int time) {
+        return String.format("%d:00", time);
+    }
+
     private void createUIComponents() {
         // Initialise & Close Connection
         try {
             Class.forName("org.postgresql.Driver");
-            dbConnection = DriverManager.getConnection(dbURL, dbUser, dbPass);
-            dbConnection.close();
-        } catch (Exception e) {
+        } catch (ClassNotFoundException e) {
             System.out.println(e);
         }
 
         // Add columns
-        String[] studentTableColumns = {"Lesson", "Class", "Date"};
+        String[] studentTableColumns = {"Classroom", "Subject", "Day", "Time"};
         DefaultTableModel studentTableModel = new DefaultTableModel(studentTableColumns, 0);
         scheduleTable = new JTable(studentTableModel);
 
-        // Add rows
-        Object[] row = new Object[3];
-        row[0] = "Mathematics";
-        row[1] = "G1";
-        row[2] = "Monday - 13:00";
+        try {
+            dbConnection = DriverManager.getConnection(dbURL, dbUser, dbPass);
+            dbStatement = dbConnection.createStatement();
+            dbResult = dbStatement.executeQuery("SELECT name, subject, day, time\n" +
+                    "FROM \"Courses\"\n" +
+                    "INNER JOIN \"Classrooms\" on \"Classrooms\".id = \"Courses\".\"classroomId\"\n" +
+                    "INNER JOIN \"Teachers\" T on T.id = \"Courses\".\"teacherId\"");
 
-        studentTableModel.addRow(row);
+            // Add rows
+            while (dbResult.next()) {
+                Object[] row = new Object[4];
+
+                row[0] = dbResult.getString(1);
+                row[1] = dbResult.getString(2);
+                row[2] = this.getDay(dbResult.getInt(3));
+                row[3] = this.getTime(dbResult.getInt(4));
+
+                studentTableModel.addRow(row);
+            }
+
+            dbStatement.close();
+            dbConnection.close();
+
+        } catch (SQLException e) {
+            System.out.printf("SQL Exception:%nError: %s%n", e.getMessage());
+        }
     }
 }
