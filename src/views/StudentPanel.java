14ec66fa263d6482b2a7793ec63e3b90aec9038a
Dionysis <pasenidis.dionisios@gmail.com>
2021-11-24 02:45:33 +0200

    Add postgresSQL connection

--- src/views/StudentPanel.java
+++ src/views/StudentPanel.java
@@ -6,31 +6,48 @@ import javax.swing.JTable;
 import javax.swing.WindowConstants;
 import javax.swing.table.DefaultTableModel;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class StudentPanel extends JFrame {
 
-  private JTable scheduleTable;
-  private JPanel studentPanel;
+    private JTable scheduleTable;
+    private JPanel studentPanel;
 
-  public StudentPanel() {
-    add(studentPanel);
-    setSize(1280, 720);
-    setResizable(false);
-    setLocationRelativeTo(null);
-    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-  }
+    private static String dbURL = "jdbc:postgresql://localhost:5432/postgres";
+    private static String dbUser = "postgres";
+    private static String dbPass = "kekw123";
+    private static Connection dbConnection;
 
-  private void createUIComponents() {
-    // Add columns
-    String[] studentTableColumns = {"Lesson", "Class", "Date"};
-    DefaultTableModel studentTableModel = new DefaultTableModel(studentTableColumns, 0);
-    scheduleTable = new JTable(studentTableModel);
+    public StudentPanel() {
+        add(studentPanel);
+        setSize(1280, 720);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 
-    // Add rows
-    Object[] row = new Object[3];
-    row[0] = "Mathematics";
-    row[1] = "G1";
-    row[2] = "Monday - 13:00";
+    private void createUIComponents() {
+        // Initialise & Close Connection
+        try {
+            Class.forName("org.postgresql.Driver");
+            dbConnection = DriverManager.getConnection(dbURL, dbUser, dbPass);
+            dbConnection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
-    studentTableModel.addRow(row);
-  }
+        // Add columns
+        String[] studentTableColumns = {"Lesson", "Class", "Date"};
+        DefaultTableModel studentTableModel = new DefaultTableModel(studentTableColumns, 0);
+        scheduleTable = new JTable(studentTableModel);
+
+        // Add rows
+        Object[] row = new Object[3];
+        row[0] = "Mathematics";
+        row[1] = "G1";
+        row[2] = "Monday - 13:00";
+
+        studentTableModel.addRow(row);
+    }
 }
