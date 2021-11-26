747218e05ec5919ee42228e07a9638c5eaafa304
Dionysis <pasenidis.dionisios@gmail.com>
2021-11-26 23:28:02 +0200

    Fix show the student name in the window  - Load the driver in Main  - Set sql variables as global

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
 import mdlaf.MaterialLookAndFeel;
 import mdlaf.themes.MaterialOceanicTheme;
 import views.StudentPanel;
 
 public class Main {
-  public static void main(String[] args)
-      throws UnsupportedLookAndFeelException {
-    UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialOceanicTheme()));
+    public static void main(String[] args)
+            throws UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialOceanicTheme()));
 
-    SwingUtilities.invokeLater(() -> {
-      StudentPanel student = new StudentPanel();
-      student.setVisible(true);
-    });
-  }
+        // Load the postgres driver
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            StudentPanel student = new StudentPanel();
+            student.setVisible(true);
+        });
+    }
 }
